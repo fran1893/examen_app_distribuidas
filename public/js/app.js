8d44971baf70f6b3b0aaa09fe89bd2958d3c5f05
Francisco Diaz <diazsalerno2@outlook.es>
2022-11-15 17:27:29 +0100

    styles updates

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ fetch(URL)
     })
     .then(data => {
         data.forEach(frase => {
-            const names = '<h2 style="color:white">' + frase.nombreAutor + '</h2>';
+            const names = '<h2>' + frase.nombreAutor + '</h2>';
             const frases = '<p>' + frase.fraseAutor + '</p>'
             const links = '<a href="' + frase.linkAutor + '">Para saber mas sobre el autor Click aquí</a>'
             const divPorAutores = '<div class="containerFrases">' + names + frases + links + '</div>'
